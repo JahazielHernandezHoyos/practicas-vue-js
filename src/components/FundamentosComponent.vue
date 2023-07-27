403c2279ebce1083cbc230f8b6e743773bd328e4
Jahaziel Hernandez Hoyos <jahazielhernandezhoyoz@gmail.com>
2023-07-27 00:05:55 -0500

    Add image distint in the FundamentosComponent for eslint

---
+++ src/components/FundamentosComponent.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        <h1>Fundamentos</h1>
+        <img src="https://images.unsplash.com/photo-1575936123452-b67c3203c357?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aW1hZ2V8ZW58MHx8MHx8fDA%3D&w=1000&q=80">
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'MiComponenteBasico'
+}
+</script>
--- src/router.js
+++ src/router.js
@@ -3,13 +3,13 @@ import Vue from 'vue';
 import VueRouter from 'vue-router';
 
 // Importa los componentes que usarás en las rutas
-import Fundamentos from './components/Fundamentos.vue';
+import FundamentosComponent from './components/FundamentosComponent.vue';
 import HelloWorld from './components/HelloWorld.vue';
 import CalendarComponent from './components/CalendarComponent.vue';
 import TailwindTest from './components/TailwindTest.vue';
 
 const routes = [
-    { path: '/fundamentos', component: Fundamentos },
+    { path: '/fundamentos', component: FundamentosComponent },
     { path: '/helloworld', component: HelloWorld },
     { path: '/calendar', component: CalendarComponent },
     { path: '/tailwindtest', component: TailwindTest }
